9ad341119947dac054da05d185e7bde18ba9034a
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2021-05-08 19:58:57 +0100

    refactor: add new theme props

---
+++ src/styles/themes/light/colors.ts
@@ -0,0 +1,10 @@
+import { ThemeColors } from '@/styles/themes';
+
+export const colors: ThemeColors = {
+  background: '#FAFBFF',
+  text: {
+    primary: '#555B6E',
+    secondary: '#A2A5AF',
+    accent: '#DE9D7B'
+  }
+};
---
+++ src/styles/themes/shared/types/theme-colors.types.ts
@@ -0,0 +1,8 @@
+export type ThemeColors = {
+  background: string;
+  text: {
+    primary: string;
+    secondary: string;
+    accent: string;
+  };
+};
