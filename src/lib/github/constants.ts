b0b614ff168e81ba2b63ee132792fd6bcf737e84
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2021-05-31 23:57:18 +0100

    refactor: update chromatic link

--- src/lib/github/constants.ts
+++ src/lib/github/constants.ts
@@ -3,5 +3,5 @@ export const GITHUB_API_URL = 'https://api.github.com';
 export const DEMO_URLS = {
   portfolio_v2: 'diogofgomespinheiro.vercel.app/',
   'portfolio-component-library':
-    '6069fd4da76d310021e27806-vxswgeskew.chromatic.com/'
+    'https://6069fd4da76d310021e27806-pazbiaedir.chromatic.com/?path=/story/atoms-button--default'
 };
